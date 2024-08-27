66b69c57f68b617a553a6bfefd55a82acb60828b
severo <oliveira.severo@gmail.com>
2024-08-27 14:45:07 -0300

    feat: :rocket: commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <p>este é meu app</p>
+    </>
+  );
+}
+
+export default App;
